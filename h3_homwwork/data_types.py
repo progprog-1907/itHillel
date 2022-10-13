062a056778427bfa8e465e606bc218d2c3649f17
imanxai93 <iman.xai93@gmail.com>
2022-10-13 04:59:27 -0400

    h3 Homework

---
+++ h3_homwwork/data_types.py
@@ -0,0 +1,57 @@
+def catalog_finder(url_list):
+    """
+    Дописать функцию, которая принимает список URL, а возвращает
+    список только тех URL, в которых есть /catalog/
+    """
+    # your code here
+    result_list = None
+    return result_list
+
+
+
+
+def get_str_center(input_str):
+    """
+    Дописать функцию, которая вернет Х символов из середины строки
+    (2 для четного кол-ва символов, 3 - для нечетного).
+    """
+    # your code here
+    output_str = None
+    return output_str
+
+
+
+
+def count_symbols(input_str):
+    """
+    Дописать функцию, которая считает сколько раз каждая из букв
+    встречается в строке, разложить буквы в словарь парами
+    {буква:количество упоминаний в строке}
+    """
+    # your code here
+    output_dict = None
+    return output_dict
+
+
+
+
+def mix_strings(str1, str2):
+    """
+    Дописать функцию, которая будет принимать 2 строки и вставлять вторую
+    в середину первой
+    """
+    # your code here
+    result_str = None
+    return result_str
+
+
+
+
+def even_int_generator():
+    """
+    Сгенерировать список из диапазона чисел от 0 до 100 и записать
+    в результирующий список только четные числа.
+    """
+    # your code here
+    even_int_list = None
+    return even_int_list
