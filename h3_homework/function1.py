31285c61e9ff1db22fd8032d24a48e3d9bee9204
imanxai93 <iman.xai93@gmail.com>
2022-10-20 08:26:11 -0400

    Homework 3 Function 1

--- h3_homework/function1.py
+++ h3_homework/function1.py
@@ -9,7 +9,13 @@ def validate_password(password):
     Если пароль не валидный, возвращает лист стрингов, в которых перечислены
     причины неуспешной проверки. Например: ["содержит запрещенные символы"]
     """
-    pass
+    if not _validate_symbols(password):
+        return ["Пароль содержит запрещенные символы"]
+    if not _validate_letters_even(password):
+        return ["Пароль содержит не четное количество букв"]
+    if not _validate_numbers_odd(password):
+        return ["Пароль содержит четное количество цифр"]
+    return True
 
 
 def _validate_symbols(input_str):
@@ -18,7 +24,9 @@ def _validate_symbols(input_str):
     Подсказка: у строк есть метод, проверяющий наличие только був и цифр
     Возвращает True\False
   """
-    pass
+    if not input_str.isalnum():
+        return False
+    return True
 
 
 def _validate_letters_even(input_str):
@@ -26,7 +34,10 @@ def _validate_letters_even(input_str):
     Проверяет строку на четное количество букв
     Возвращает True\False
     """
-    pass
+    len_letter = len([let for let in input_str if let.isalpha()])
+    if len_letter % 2:
+        return False
+    return True
 
 
 def _validate_numbers_odd(input_str):
@@ -34,4 +45,12 @@ def _validate_numbers_odd(input_str):
     Проверяет строку на нечетное количество цифр
     Возвращает True\False
     """
-    pass
+    len_number = len([num for num in input_str if num.isdigit()])
+    if not len_number % 2:
+        return False
+    return True
+
+
+passw = "i3l2e9fb7q11!"
+data = validate_password(passw)
+print(data)
